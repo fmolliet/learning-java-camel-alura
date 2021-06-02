384bd2a363eb68c21bffafa5fc3f21ba71f38796
fmolliet <fmolliet@hotmail.com>
2021-06-01 23:49:24 -0300

    Adicionado header dinamico, marshal, e split

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,6 +1,7 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
@@ -17,10 +18,14 @@ public class RotaPedidos {
 			public void configure() throws Exception {
 				
 				from("file:pedidos?delay=5s&noop=true")
-					.setHeader("CamelFileName", simple("${file:name.noext}.json")) // para usar nomes constants só usar metodo constant
+					.split() // divide o conteudo
+						.xpath("/pedido/itens/item") // Separa pelos items do pedido
+					.filter() // filtra o conteudo
+						.xpath("/item/formato[text()='EBOOK']") // usa o xpath para buscar o texto do formato 
+						// Vamos tirar o pedido/itens pois depois que ele dividiu formou um novo objeto
+					.marshal().xmljson() // transforma xml em JSON ou dados em memoria para outro formado e o contrario é unmarshal
 					.log("${id} - ${body}")
-					.marshal().xmljson() // transforma xml em JSON
-					.log("${body}")
+					.setHeader(Exchange.FILE_NAME, simple("${file:name.noext}-${header.CamelSplitIndex}.json")) // para usar nomes constants só usar metodo constant
 				.to("file:saida");
 				
 			}
